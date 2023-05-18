4cfaa025b60747a23fea35205fb3ab4a8093678f
lub <bamert@protonmail.com>
2023-05-18 21:02:54 +0200

    First unit test of camunda workflow

--- bet-it/zeebe-addon/src/test/java/ch/unisg/ics/edpo/addon/TestContractProcess.java
+++ bet-it/zeebe-addon/src/test/java/ch/unisg/ics/edpo/addon/TestContractProcess.java
@@ -4,6 +4,7 @@ import ch.unisg.ics.edpo.addon.service.AddonConsumerService;
 import ch.unisg.ics.edpo.shared.Topics;
 import ch.unisg.ics.edpo.shared.bank.UserCheck;
 import ch.unisg.ics.edpo.shared.contract.ContractData;
+import ch.unisg.ics.edpo.shared.game.GameValidCheck;
 import ch.unisg.ics.edpo.shared.kafka.KafkaConsumerFactoryHashMap;
 import ch.unisg.ics.edpo.shared.kafka.KafkaMapProducer;
 import io.camunda.zeebe.client.ZeebeClient;
@@ -17,6 +18,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
@@ -57,21 +59,26 @@ public class TestContractProcess {
     ArgumentCaptor<String> captorKey;
 
     @Test
-    public void testFailedUserCheck() throws Exception {
+    public void testHappyPath() throws Exception {
 
         ContractData contractData = new ContractData("gameid123", 2.0, "lukas", true, "123345");
         ProcessInstanceEvent instance = startCamunda(Topics.Contract.CONTRACT_REQUESTED, contractData.toMap());
         waitForProcessInstanceHasPassedElement(instance, "BankValidityCheckToKafka");
-        UserCheck userCheck = new UserCheck("lukas", UserCheck.UserCheckResult.REJECTED);
+        waitForProcessInstanceHasPassedElement(instance, "game-validity-check-send");
+        UserCheck userCheck = new UserCheck("lukas", UserCheck.UserCheckResult.APPROVED);
+        GameValidCheck gameValidCheck = new GameValidCheck(contractData.getGameId(), GameValidCheck.GameValidStatus.APPROVED);
+
         sendToCamunda(Topics.Bank.User.CHECK_RESULT, userCheck.getCorrelationKey(), userCheck.toMap());
+        sendToCamunda(Topics.Game.GAME_VALID_FOR_CONTRACT_RESULT, gameValidCheck.getCorrelationKey(), gameValidCheck.toMap());
         waitForProcessInstanceHasPassedElement(instance, "Wait_User_Check_Element");
-        waitForProcessInstanceHasPassedElement(instance, "send_contract_rejected");
-        assertThat(instance).hasPassedElement("BankValidityCheckToKafka");
+        waitForProcessInstanceHasPassedElement(instance, "game_check_received");
+        //waitForProcessInstanceHasPassedElement(instance,"SendContractAccepted");
+        //assertThat(instance).hasPassedElement("SendContractAccepted");
 
-        verify(kafkaMapProducer, times(2)).sendMessage(captorMessage.capture(), captorTopic.capture(), captorKey.capture());
-        Assertions.assertEquals(captorTopic.getAllValues().get(0), Topics.Bank.User.CHECK_REQUEST);
-        Assertions.assertEquals(captorTopic.getAllValues().get(1), Topics.Contract.CONTRACT_REJECTED);
 
+        verify(kafkaMapProducer, times(3)).sendMessage(captorMessage.capture(), captorTopic.capture(), captorKey.capture());
+        System.out.println(captorTopic.getAllValues());
+        System.out.println(captorMessage.getAllValues());
 
         assertThat(instance).
                 hasNoIncidents()
