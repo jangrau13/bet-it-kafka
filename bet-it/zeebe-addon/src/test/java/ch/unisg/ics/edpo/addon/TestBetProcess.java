d7773c1c3d74599110dd76ada9b367c0f6b0d21c
lub <bamert@protonmail.com>
2023-05-24 13:20:49 +0200

    Some little workflow changes

--- bet-it/zeebe-addon/src/test/java/ch/unisg/ics/edpo/addon/TestBetProcess.java
+++ bet-it/zeebe-addon/src/test/java/ch/unisg/ics/edpo/addon/TestBetProcess.java
@@ -13,7 +13,11 @@ import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
 import io.camunda.zeebe.process.test.api.ZeebeTestEngine;
 import io.camunda.zeebe.process.test.filters.RecordStream;
+import io.camunda.zeebe.process.test.inspections.InspectionUtility;
+import io.camunda.zeebe.process.test.inspections.ProcessInstanceInspections;
+import io.camunda.zeebe.process.test.inspections.model.InspectedProcessInstance;
 import io.camunda.zeebe.spring.test.ZeebeSpringTest;
+import org.camunda.feel.FeelEngine;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
@@ -22,9 +26,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.TimeoutException;
 
 import static ch.unisg.ics.edpo.addon.testUtils.Utils.sendToCamunda;
 import static ch.unisg.ics.edpo.addon.testUtils.Utils.startCamunda;
@@ -37,11 +44,13 @@ import static org.mockito.Mockito.verify;
 @SpringBootTest
 @ZeebeSpringTest
 public class TestBetProcess {
-    private ZeebeTestEngine engine;
 
     /**
      * Ignore this zeebe error
      */
+
+    @Autowired
+    ZeebeTestEngine zeebeTestEngine;
     @Autowired
     private ZeebeClient zeebe;
     private RecordStream recordStream;
@@ -65,7 +74,7 @@ public class TestBetProcess {
     ArgumentCaptor<String> captorKey;
 
     @Test
-    public void testHappy(){
+    public void testHappy() throws InterruptedException, TimeoutException {
         ContractData contractData = new ContractData("gameid123", 2.1, "lukas", true, "123345");
         Bet bet = new Bet(contractData, "betId123", "jan", 500.0, LocalDateTime.now());
         ProcessInstanceEvent instance = startCamunda(Topics.Bet.BET_REQUESTED, bet.toMap(), zeebe);
@@ -76,16 +85,23 @@ public class TestBetProcess {
         sendToCamunda(Topics.Bank.Freeze.FREEZE_RESULT, bet.getContractorName(), secondFreezeResponse.toMap(), zeebe);
         waitForProcessInstanceHasPassedElement(instance, "freeze_contractor");
         waitForProcessInstanceHasPassedElement(instance, "accept_bet_send");
-        GameObject gameObject = new GameObject(contractData.getGameId(), "team1", "team2", GameObject.GameState.ENDED, true);
+        GameObject gameObject = new GameObject(contractData.getGameId(), "team1", "team2", GameObject.GameState.ENDED, true, "");
         sendToCamunda(Topics.Game.GAME_ENDED, gameObject.getGameId(), gameObject.toMap(), zeebe);
 
+        waitForProcessInstanceHasPassedElement(instance, "game_ended_element");
         TransactionEvent transactionEvent = new TransactionEvent(bet.getBuyerName(), bet.getContractorName(),
                 bet.getRatio() * bet.getAmountBought(), TransactionEvent.TRANSACTION_STATUS.DONE, bet.getBuyerName() + bet.getContractorName());
-        // this is too early
         sendToCamunda(Topics.Bank.Transaction.TRANSACTION_RESULT, transactionEvent.getCorrelationId(), transactionEvent.toMap(), zeebe);
+
         waitForProcessInstanceHasPassedElement(instance, "pay_contractor");
-        waitForProcessInstanceHasPassedElement(instance, "game_ended_element");
-        verify(kafkaMapProducer, times(3)).sendMessage(captorMessage.capture(), captorTopic.capture(), captorKey.capture());
+
+        FreezeEvent freezeEvent = new FreezeEvent(bet.getBuyerName(), -bet.getAmountBought() * bet.getRatio(), FreezeEvent.STATUS.ACCEPTED);
+        sendToCamunda(Topics.Bank.Freeze.FREEZE_RESULT,freezeEvent.getUser(), freezeEvent.toMap(), zeebe );
+        waitForProcessInstanceHasPassedElement(instance, "unfreeze_buyer_element");
+        FreezeEvent freezeEvent1 = new FreezeEvent(bet.getContractorName(), bet.getAmountBought(), FreezeEvent.STATUS.ACCEPTED);
+        sendToCamunda(Topics.Bank.Freeze.FREEZE_RESULT, freezeEvent1.getCorrelationId(), freezeEvent1.toMap(), zeebe);
+
+        verify(kafkaMapProducer, times(6)).sendMessage(captorMessage.capture(), captorTopic.capture(), captorKey.capture());
 
         List<Map<String, Object>> allMsg = captorMessage.getAllValues();
 
