1a6b4aa85f814004032375b26f21f94b1eb4e90e
jangrau13 <jangrau13@gmail.com>
2023-05-25 00:45:03 +0200

    camunda.game.ended is working

--- bet-it/fraud-backend/server.js
+++ bet-it/fraud-backend/server.js
@@ -14,6 +14,95 @@ enableWs(app)
 
 const producer = kafka.producer()
 
+async function createTopic(admin, topic) {
+    await admin.createTopics({
+        topics: [
+            {
+                topic,
+                numPartitions: 1,
+                replicationFactor: 1,
+            },
+        ],
+    });
+}
+
+async function initializeTopic() {
+    try {
+        await producer.connect();
+        const admin = kafka.admin();
+        await admin.connect();
+
+        // const topics = ['game.dot.started', 'game.dot.hit', 'game.dot.miss', 'game.dot.friendlyfire', 'game.player', 'camunda.game.started', 'game.dot.spawn', 'game.published'];
+        //
+        // await topics.forEach(topic => {
+        //     createTopic(admin, topic)
+        // })
+
+
+        // Create the topic with the desired configuration
+        await admin.createTopics({
+            topics: [
+                {
+                    topic: 'game.dot.started',
+                    numPartitions: 1,
+                    replicationFactor: 1,
+                },
+                {
+                    topic: 'game.dot.hit',
+                    numPartitions: 1,
+                    replicationFactor: 1,
+                },
+                {
+                    topic: 'game.dot.miss',
+                    numPartitions: 1,
+                    replicationFactor: 1,
+                },
+                {
+                    topic: 'game.dot.friendlyfire',
+                    numPartitions: 1,
+                    replicationFactor: 1,
+                },
+                {
+                    topic: 'game.player',
+                    numPartitions: 1,
+                    replicationFactor: 1,
+                },
+                {
+                    topic: 'game.dot.spawn',
+                    numPartitions: 1,
+                    replicationFactor: 1,
+                },
+                {
+                    topic: 'camunda.game.started',
+                    numPartitions: 1,
+                    replicationFactor: 1,
+                },
+                {
+                    topic: 'game.published',
+                    numPartitions: 1,
+                    replicationFactor: 1,
+                },
+                {
+                    topic: 'camunda.game.ended',
+                    numPartitions: 1,
+                    replicationFactor: 1,
+                },
+                {
+                    topic: 'game.dot.ended',
+                    numPartitions: 1,
+                    replicationFactor: 1,
+                }
+            ],
+        });
+
+        await admin.disconnect();
+    } catch (error) {
+        console.error('Error initializing topic:', error);
+        process.exit(1);
+    }
+}
+
+
 const testID = '';
 
 //if a dot has been hit
@@ -91,12 +180,21 @@ function handleWebSocketRequest(ws, topic) {
 
 app.use(cors()); // Enable CORS for all routes
 
-app.listen(3001, () => {
-    console.log("Server started");
-    producer.connect()
+async function startServer() {
+    try {
+        await producer.connect();
+        await initializeTopic();
+        // Start your server logic here
+        app.listen(3001, () => {
+            console.log('Server started on port 3001');
+        });
+    } catch (error) {
+        console.error('Error starting server:', error);
+        process.exit(1);
+    }
+}
 
-    // Additional code to execute on server start up
-});
+startServer()
 
 process.on("SIGINT", () => {
     server.close(() => {
