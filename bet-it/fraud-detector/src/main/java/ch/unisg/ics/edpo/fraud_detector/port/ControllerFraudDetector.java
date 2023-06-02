d8f461871d3beedd94efc27e47e9e8db5d355da0
lub <bamert@protonmail.com>
2023-06-02 15:47:15 +0200

    Updated README.md

--- bet-it/fraud-detector/src/main/java/ch/unisg/ics/edpo/fraud_detector/port/ControllerFraudDetector.java
+++ bet-it/fraud-detector/src/main/java/ch/unisg/ics/edpo/fraud_detector/port/ControllerFraudDetector.java
@@ -1,6 +1,10 @@
 package ch.unisg.ics.edpo.fraud_detector.port;
 
+import ch.unisg.ics.edpo.shared.avroschema.SendPayment;
+import ch.unisg.ics.edpo.shared.kafka.avro.KafkaPaymentSentProducer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/detector")
 public class ControllerFraudDetector {
 
+    private final KafkaPaymentSentProducer kafkaPaymentSentProducer;
 
+    public ControllerFraudDetector(KafkaPaymentSentProducer kafkaPaymentSentProducer) {
+        this.kafkaPaymentSentProducer = kafkaPaymentSentProducer;
+    }
 
+    @GetMapping("/test")
+    public ResponseEntity<String> getStatus() {
+        kafkaPaymentSentProducer.sendMessage(new SendPayment(22.0, "hi", "you"));
+        return ResponseEntity.ok("Status OK");
+    }
 
 }
--- bet-it/shared/src/main/java/ch/unisg/ics/edpo/shared/kafka/avro/KafkaConsumerPaymentSent.java
+++ bet-it/shared/src/main/java/ch/unisg/ics/edpo/shared/kafka/avro/KafkaConsumerPaymentSent.java
@@ -3,16 +3,19 @@ package ch.unisg.ics.edpo.shared.kafka.avro;
 import ch.unisg.ics.edpo.shared.avroschema.SendPayment;
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 
 import java.util.HashMap;
@@ -26,7 +29,7 @@ public class KafkaConsumerPaymentSent {
     @Value("${spring.kafka.consumer.bootstrap-servers}")
     private String bootstrapServers;
 
-
+    private final String schemaRegistryUrl = "http://schema-registry:8085";
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, SendPayment> consumerSendPaymentFactory(final ConsumerFactory<String, SendPayment> consumerFactory) {
         final ConcurrentKafkaListenerContainerFactory<String, SendPayment> factory = new ConcurrentKafkaListenerContainerFactory<>();
@@ -44,8 +47,28 @@ public class KafkaConsumerPaymentSent {
         config.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
         config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, KafkaAvroDeserializer.class);
         config.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true);
+        config.put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
+        config.put(KafkaAvroDeserializerConfig.AUTO_REGISTER_SCHEMAS, true);
         return new DefaultKafkaConsumerFactory<>(config);
     }
 
+    /**
+     * About producing from here
+     */
+    @Bean
+    public KafkaTemplate<String, SendPayment> kafkaPaymentSentProducerTemplate(final ProducerFactory<String, SendPayment> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    public ProducerFactory<String, SendPayment > producerFactory() {
+        final Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        config.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG,schemaRegistryUrl );
+        config.put(KafkaAvroSerializerConfig.AUTO_REGISTER_SCHEMAS, true);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
 
 }
