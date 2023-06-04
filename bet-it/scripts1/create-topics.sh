d209c438eacd521e764e8c629549a00bc14f47df
jangrau13 <jangrau13@gmail.com>
2023-06-04 17:56:53 +0200

    new readme

--- bet-it/scripts1/create-topics.sh
+++ bet-it/scripts1/create-topics.sh
@@ -8,5 +8,8 @@ kafka-topics --create --bootstrap-server kafka:9092 --topic camunda.bet.requeste
 kafka-topics --create --bootstrap-server kafka:9092 --topic camunda.add-user --replication-factor 1 --partitions 1
 kafka-topics --create --bootstrap-server kafka:9092 --topic camunda.game.ended --replication-factor 1 --partitions 1
 kafka-topics --create --bootstrap-server kafka:9092 --topic camunda.twofa --replication-factor 1 --partitions 1
+kafka-topics --create --bootstrap-server kafka:9092 --topic camunda.user.check-result --replication-factor 1 --partitions 1
+kafka-topics --create --bootstrap-server kafka:9092 --topic camunda.TwoFactorSuccessEvent --replication-factor 1 --partitions 1
+
 
 sleep infinity
